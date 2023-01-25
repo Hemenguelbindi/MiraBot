72441c1818e5204e45cddba2cddb2eb3ab6402f6
Victor Karpov@ <nilafe4@gmail.com>
2023-01-25 16:45:43 +0500

    fix this shite

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -9,16 +9,17 @@ def register_admin_handler(dp: Dispatcher):
 
 # Commad Start from admins 
 async def send_command_start_admin(message: types.Message):
-    for admin in get_admins():
-        if admin == get_admins()[0]:
+    admins = get_admins()
+    for admin in admins:
+        if admin == admins[0]:
             await message.bot.send_animation(
-                chat_id=message.chat.id,
+                chat_id=admin,
                 animation=choise_random_gif("hello"),
                 caption=ANSWER_ADMIN["Victor"])
             await message.delete()
         # если по другому тогда отправить другому админу
-        if admin == get_admins()[1]:
-            await message.bot.send_animation(chat_id=message.chat.id,
+        if admin == admins[1]:
+            await message.bot.send_animation(chat_id=admin,
                                          animation=choise_random_gif("hello"),
                                          caption=ANSWER_ADMIN["Kristina"])
             await message.delete()

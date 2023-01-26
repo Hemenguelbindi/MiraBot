590c66193d096834ce89b0598827c8691cf79b26
Victor Karpov@ <nilafe4@gmail.com>
2023-01-26 09:55:31 +0500

    fix

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from keyboards.set_menu import set_main_menu
 from handlers import register_admin_handler, register_privat_user_handlers, register_user_handlers
+from handlers_tasks import send_weather, send_active_care
 from config_data import Config, load_config
 
 
 logger.add("logs/{time}.log", format="{time} {level} {message}", rotation="1 week", compression="zip")
 
 config: Config = load_config()
+
 
 # Фнукция для регистрации всех хэндлеров
 def register_all_handlers(dp: Dispatcher) -> None:
@@ -24,7 +26,6 @@ def register_all_handlers(dp: Dispatcher) -> None:
 
 # Функция конфигурирования и запуска бота
 async def main():
-        
     logger.info("Start bot")
     logger.info("Get configuration")
     config: Config = load_config()
@@ -33,11 +34,12 @@ async def main():
     # Инициализируем бот и диспетчер
     mira_bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
     dp: Dispatcher = Dispatcher(mira_bot, storage=storage)
+    logger.info("Init tasks...")
     scheduler = AsyncIOScheduler(timezone="Asia/Yekaterinburg")
-    scheduler.add_job(send_weather, trigger='cron', hour='9', minute='50',
+    scheduler.add_job(send_weather, trigger='cron', hour='10', minute='10',
                       start_date=datetime.now(),
                       kwargs={'bot': mira_bot})
-    scheduler.add_job(send_active_care, trigger='cron', hour="9", minute='50',
+    scheduler.add_job(send_active_care, trigger='cron', hour="10", minute='10',
                       start_date=datetime.now(),
                       kwargs={'bot': mira_bot})
     scheduler.start()
--- handlers_tasks/__init__.py
+++ handlers_tasks/__init__.py
@@ -1,1 +1,1 @@
-from .heandlers_task_cron import send_weather, send_active_care
+from .handlers_task_cron import send_weather, send_active_care

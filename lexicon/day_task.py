72b9c49b89dd20c118e47fdbc8b6bace279c61e8
Victor Karpov <nilafe4@gmail.com>
2023-02-21 14:20:43 +0500

    create lists random traning

--- lexicon/day_task.py
+++ lexicon/day_task.py
@@ -1,13 +1,48 @@
-active_and_care = {
-    "25.01.2023": "25 января - 12500 шагов https://youtu.be/H6iHamGsr44",
-    "26.01.2023": "26 января - интервальная https://youtu.be/M1vkvTGrIpo",
-    "27.01.2023": "27 января - 12500 шагов https://youtu.be/H6iHamGsr44",
-    "28.01.2023": "28 января - 25000 шагов https://youtu.be/H6iHamGsr44",
-    "29.01.2023": "29 января - 12500 шагов https://youtu.be/H6iHamGsr44",
-    "30.01.2023": "30 января - круговая https://youtu.be/35ZVvGeAneo",
-    "31.01.2023": "31 января - 12500 шагов https://youtu.be/H6iHamGsr44",
-    "01.02.2023": "1 февраля - 12500 шагов https://youtu.be/H6iHamGsr44",
-    "02.02.2023": "2 февраля - лимфодренаж https://youtu.be/ckk5LW-62mw + 18000 шагов",
-    "03.02.2023": "3 февраля - 12500 шагов https://youtu.be/H6iHamGsr44",
-}
 
+
+warm_up_tranging: list = list(
+    "https://youtu.be/2ax28C-hWPY",
+    "https://youtu.be/DJcG7AUuzBA",
+    "https://www.youtube.com/watch?v=5vfqX6wGTWQ",
+    "https://www.youtube.com/watch?v=qRXs-c3LHr4",
+    "https://www.youtube.com/watch?v=xit7yDZguYY",
+    "https://www.youtube.com/watch?v=YArvGEk8c6c",
+)
+hands_traning: list = list(
+    "https://youtu.be/eSwsiODvzeA",
+    "https://www.youtube.com/watch?v=zXLwlRGfCMQ",
+    "https://www.youtube.com/watch?v=Lu0ZVXcaiPI",
+    "https://www.youtube.com/watch?v=E5_SDJXuGkg",
+)
+
+press_traning: list = list(
+    "https://www.youtube.com/watch?v=v2ruAhNGiUE&t=124s",
+    "https://www.youtube.com/watch?v=p4OLS1iByYA",
+    "https://www.youtube.com/watch?v=8dPiN8I7q_U",
+    "https://youtu.be/2nlSPvy20JI",
+)
+all_body_traning: list = list(
+    "https://www.youtube.com/watch?v=Ex1XOU1wr64",
+    "https://www.youtube.com/watch?v=23WTBzutLJE",
+    "https://www.youtube.com/watch?v=j9j9TIMu6xQ",
+    "https://www.youtube.com/watch?v=IbdS_z2Pu4c",
+)
+cardios_traning: list = list(
+    "https://www.youtube.com/watch?v=ChkB8xFLPfM",
+    "https://www.youtube.com/watch?v=vHHp56rVygk",
+    "https://www.youtube.com/watch?v=miPJFOj-9m0",
+    "https://www.youtube.com/watch?v=fG-p3lgTOa8",
+)
+hitch_traning: list = list(
+    "https://www.youtube.com/watch?v=eSzUNyWh0zg",
+    "https://www.youtube.com/watch?v=y3DjEepGlkQ",
+    "https://www.youtube.com/watch?v=qRXs-c3LHr4&t=23s",
+    "https://www.youtube.com/watch?v=h5nKxK8bmB0",
+)
+stretching_traning: list = list(
+    "https://www.youtube.com/watch?v=CHhQb_mUNcU",
+    "https://www.youtube.com/watch?v=3ZjkYjtvrek",
+    "https://www.youtube.com/watch?v=3YP6Ldxj71U",
+    "https://www.youtube.com/watch?v=X1BbolkPf0I",
+    "https://www.youtube.com/watch?v=4JZCt9Ex-ow",
+)
